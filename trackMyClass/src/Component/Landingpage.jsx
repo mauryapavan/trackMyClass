5b1474837b4107f33556e9e9c16f8ae7a3d5a763
mauryapavan <mauryapavan667@gmail.com>
2025-09-18 21:15:31 +0530

    Update Landingpage.jsx

--- trackMyClass/src/Component/Landingpage.jsx
+++ trackMyClass/src/Component/Landingpage.jsx
@@ -4,10 +4,13 @@ import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
     let navigate=useNavigate();
+      async function teachers(params) {
+        window.location.href="https://track-my-class.onrender.com"
+    }
     return (
         <div className="m-5 p-5 min-h-150 grid  grid-cols-1 items-center justify-items-center-safe text-center    backdrop-blur-md backdrop-brightness-110"  >
             <div className="p-5 flex ">
-                <div className="m-5">
+                <div className="m-5" onClick={teachers}>
                     <button  className=" p-2  rounded-md bg-green-900 pointer " style={{ color: "whitesmoke" }}> <i><a href="">Teacher's Login</a></i></button>
 
                 </div>
