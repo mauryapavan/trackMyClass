abf349bd1d449d2880373d42869228b359d8ecea
mauryapavan <mauryapavan667@gmail.com>
2025-09-18 16:38:45 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import SuggestTask from "./suggesttask/task.js";
 import SuggestCourse from "./SuggestCourse/SuugesCourse.js";
+import methodOverride from 'method-override';
 
 
 
@@ -14,12 +15,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4444;
 
-app.use(cors({
-  origin: "https://trackmyclass-1-0eou.onrender.com",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-app.options("/*", cors());
+app.use(cors());
+app.use(methodOverride('_method'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
